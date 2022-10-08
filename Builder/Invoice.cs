41dc790a4bacc2da58fff03b90c4743993b34a63
lukasPWR <jnowicki91@gmail.com>
2022-10-08 15:02:02 +0200

    Builder

---
+++ Builder/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    internal class Invoice
+    {
+        public string Number { get; set; }
+        public DateTime Date { get; set; }
+        public string Vendor { get; set; }
+        public string Vendee { get; set; }
+        public IEnumerable<string> LineItems { get; set; }
+        public string Note { get; set; }
+
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Builder;
+
+Console.WriteLine("Hello, World!");
+
+var builder = new InvoiceBuilder();
+var invoice =builder.SetDate(new DateTime(2020,1,1))
+    .SetInvoiceNumber("1")
+    .SetVendor("Google")
+    .SetVendee("Vendee")
+    .SetLinetItems(new List<string>(){"line item1", "line item2"})
+    .Build();
+
+var builder2 = new Director();
+var invoice2 = builder2.CreateMonthInvoice(new DateTime(2000, 1, 1));
+Console.WriteLine(invoice.Vendee);
+Console.WriteLine(invoice.Vendor);
+Console.WriteLine(invoice2.Date);
