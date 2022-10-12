3a0b9173473a6d7bf379c777159309be70d9d747
lukasPWR <jnowicki91@gmail.com>
2022-10-12 13:07:53 +0200

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+
+var cfg =  Configuration.GetInstance();
+var cfg2 = Configuration.GetInstance();
+
+if (ReferenceEquals(cfg, cfg2))
+{
+    Console.WriteLine("configuration is a singleton");
+}
